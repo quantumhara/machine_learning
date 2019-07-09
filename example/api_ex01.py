c3c9330dfaa6d4cf0a6fdba0b9da05519fd320b9
Schun Uechi <quantum2006hara@gmail.com>
2019-07-08 22:56:55 -0700

    api

---
+++ example/api_ex01.py
@@ -0,0 +1,18 @@
+import json
+import requests
+# Make a get request to get the latest position of the international space station from the opennotify api.
+response = requests.get("http://api.open-notify.org/iss-now.json")
+# Print the status code of the response.
+
+print("url: url属性")
+print(response.url)
+print("ステータスコード: status_code属性")
+print(response.status_code)
+print("エンコーディング: encoding属性")
+print(response.encoding)
+print("レスポンスヘッダ: headers属性")
+print(response.headers)
+print("テキスト: text属性")
+print(response.text)
+print("バイナリデータ: content属性")
+print(response.content)
